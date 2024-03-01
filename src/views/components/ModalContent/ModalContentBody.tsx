dda2ddf6d3c31a74be4d1db582b23f2820bbdd6a
Rodrigo <rodrigo_moraes_@hotmail.com>
2024-03-01 19:25:51 -0300

    feat(modalcontent): implemeta componente 'ModalComponents'
    Componente segue composite pattern de desenvolvimento

---
+++ src/views/components/ModalContent/ModalContentBody.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react";
+
+interface ModalContentBodyProps {
+  children: ReactNode;
+}
+
+export function ModalContentBody({ children }: ModalContentBodyProps) {
+  return (
+    <div className="flex items-center justify-between bg-gray-50 px-4 py-3">
+      <div className="flex flex-1 items-center">{children}</div>
+    </div>
+  );
+}
---
+++ src/views/components/ModalContent/ModalContentHeadingInfo.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { CheckIcon, ExclamationTriangleIcon } from "@heroicons/react/20/solid";
+
+export function ModalContentHeaderInfo() {
+  return (
+    <div className="flex hidden h-12 w-12 items-center justify-center rounded-full bg-none"></div>
+  );
+}
